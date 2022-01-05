e451f308b8254974adf086a0582a4e297a37529f
PoisonIceIvy <96007314+PoisonIceIvy@users.noreply.github.com>
2022-01-05 13:37:15 +0000

    help

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click',(
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }))
+
+var buttonOpen = document.querySelector('.button-open');
+var menu = document.querySelector('.menu');
+buttonOpen.addEventListener('click', () => {
+    buttonOpen.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+document.querySelectorAll('menu').forEach(n => n.addEventListener('click',() => {
+    buttonOpen.classList.remove('active');
+    menu.classList.remove('active');
+} ))
